defe8a862831624d4ebd56c1c92f2d5fd75e6e51
Javier <jachavez6@gmail.com>
2021-03-14 19:20:26 -0400

    Add button to delete files after they've been used
    This will also clear the contents in case the wrong file has been
    uploaded or any other reason

--- reels_app/main/routes.py
+++ reels_app/main/routes.py
@@ -1,10 +1,27 @@
-from flask import Blueprint, render_template
+import os
+
+from flask import Blueprint, request, render_template
+from flask.helpers import url_for
+from werkzeug.utils import redirect
 
 main = Blueprint('main', __name__)
 
 
-@main.route('/')
+@main.route('/', methods=['GET', 'POST'])
 def index():
     """Render the index/home page."""
+    if request.method == 'POST':
+        for item in os.listdir('csvs'):
+            os.remove(os.path.join('csvs', item))
+
+        for item in os.listdir('uploads/photos'):
+            print(os.path.join('uploads/photos', item))
+            os.remove(os.path.join('uploads/photos', item))
+
+        for item in os.listdir('uploads/spreadsheets'):
+            print(os.path.join('uploads/spreadsheets', item))
+            os.remove(os.path.join('uploads/spreadsheets', item))
+
+        return redirect(url_for('main.index'))
 
     return render_template('index.html', title='Home')
