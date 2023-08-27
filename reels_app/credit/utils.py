d1fa75de87c986822882eb2ff772baf2116019ff
Javier <jachavez6@gmail.com>
2023-08-27 12:46:01 -0400

    Write crew method

--- reels_app/credit/utils.py
+++ reels_app/credit/utils.py
@@ -19,11 +19,11 @@ class Film:
         self.title = title
         self.imdb_id = imdb_id
         self.tmdb_id = None  # done
-        self.response = None  # done
+        self.movie_info = None  # done
         self.directors = []
         self.writers = []
         self.producers = []
-        self.countries = []
+        self.countries = []  # done
         self.languages = []  # done
         self.release_date = None  # done
         self.runtime = None  # done
@@ -34,35 +34,49 @@ class Film:
         response = tmdb.Find(self.imdb_id).info(external_source='imdb_id')
         self.tmdb_id = response['movie_results'][0]['id']
 
-    def get_response(self):
-        self.response = tmdb.Movies(self.tmdb_id).info()
+    def get_movie_info(self):
+        self.movie_info = tmdb.Movies(self.tmdb_id).info()
 
     def get_release_date(self):
-        self.release_date = self.response['release_date'][:4]
+        self.release_date = self.movie_info['release_date'][:4]
 
     def get_runtime(self):
-        self.runtime = self.response['runtime']
+        self.runtime = self.movie_info['runtime']
 
     def get_countries(self):
-        production_countries = self.response['production_countries']
+        production_countries = self.movie_info['production_countries']
         countries = []
         for country in production_countries:
             countries.append(country['name'])
         self.countries = countries
 
     def get_languages(self):
-        spoken_languages = self.response['spoken_languages']
+        spoken_languages = self.movie_info['spoken_languages']
         languages = []
         for language in spoken_languages:
             languages.append(language['english_name'])
         self.languages = languages
 
     def get_genres(self):
-        tmdb_genres = self.response['genres']
+        tmdb_genres = self.movie_info['genres']
         genres = []
         for genre in tmdb_genres:
             genres.append(genre['name'])
         self.genres = genres
+
+    def get_crew(self):
+        """Return the director(s), screenwriter(s), and producer(s) of the
+        queried film."""
+
+        crew = tmdb.Movies(self.tmdb_id).credits()['crew']
+
+        for crew_member in crew:
+            if crew_member['job'] == 'Director':
+                self.directors.append(crew_member['name'])
+            if crew_member['job'] == 'Screenplay':
+                self.writers.append(crew_member['name'])
+            if crew_member['job'] == 'Producer':
+                self.producers.append(crew_member['name'])
 
 
 def build_film_list(directory):
@@ -87,12 +101,13 @@ def build_film_list(directory):
     for film in films:
         # Each of the class's attributes will be populated in this loop
         film.get_tmdb_id()
-        film.get_response()
+        film.get_movie_info()
         film.get_release_date()
         film.get_runtime()
         film.get_countries()
         film.get_languages()
         film.get_genres()
+        film.get_crew()
         print(f'Title: {film.title}')
         print(f'TMDB ID: {film.tmdb_id}')
         print(f'Release Date: {film.release_date}')
@@ -100,35 +115,7 @@ def build_film_list(directory):
         print(f'Countries: {film.countries}')
         print(f'Languages: {film.languages}')
         print(f'Genres: {film.genres}')
+        print(f'Directors: {film.directors}')
+        print(f'Writers: {film.writers}')
+        print(f'Producers: {film.producers}')
         print('-------')
-
-
-def get_year(movie_id):
-    """Return the release year of the queried film."""
-    movie = tmdb.Movies(movie_id)
-    # print(movie.info()['release_date'][:4])
-    return movie.info()['release_date'][:4]
-
-
-def get_crew(movie_id):
-    """Return the director(s), screenwriter(s), and producer(s) of the queried
-    film."""
-    movie = tmdb.Movies(movie_id)
-
-    crew_list = movie.credits()['crew']
-
-    directors = []
-    screenwriters = []
-    producers = []
-
-    for crew in crew_list:
-        # get director
-        if crew['job'] == 'Director':
-            directors.append(crew['name'])
-        if crew['job'] == 'Story':
-            screenwriters.append(crew['name'])
-        if crew['job'] == 'Producer':
-            producers.append(crew['name'])
-
-    print(directors, screenwriters, producers)
-    return directors, screenwriters, producers
