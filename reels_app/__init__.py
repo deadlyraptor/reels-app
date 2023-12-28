91192b4dccb64541f143724f2f26678398fa1b39
Javier <jachavez6@gmail.com>
2023-12-28 15:10:08 -0500

    Remove unnecessary pdf routes and Blueprint
    Since refactoring the code for splitting the box office report and
    renaming Deluxe invoices to use the new generic UploadView and
    DownloadView, the old code is no longer needed so it's gone!

--- reels_app/__init__.py
+++ reels_app/__init__.py
@@ -6,7 +6,6 @@ from flask_dropzone import Dropzone
 from reels_app.main.routes import main, DownloadView, UploadView
 from reels_app.credit.routes import credit
 from reels_app.genres.routes import genre
-from reels_app.pdf.routes import pdf
 from reels_app.po.routes import po
 from reels_app.rename.routes import rename
 from reels_app.spreadchimp.routes import spreadchimp
@@ -40,7 +39,6 @@ def register_blueprints(app):
     app.register_blueprint(main)
     app.register_blueprint(credit)
     app.register_blueprint(genre)
-    app.register_blueprint(pdf)
     app.register_blueprint(po)
     app.register_blueprint(rename)
     app.register_blueprint(spreadchimp)
